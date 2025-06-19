6562ae65f7ac69d43ca07728dd278032cf775a22
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-19 22:13:56 +0300

    Update ex05.sql

--- day00/ex05.sql
+++ day00/ex05.sql
@@ -1,4 +1,4 @@
 SELECT 
-    (SELECT name FROM person WHERE id = person_order.person_id) AS name
-FROM person_order 
-WHERE menu_id IN (13, 14, 18) AND order_date = '2022-01-07';
+    (SELECT name FROM person WHERE id = po.person_id) AS name
+FROM person_order po
+WHERE po.menu_id IN (13, 14, 18) AND po.order_date = '2022-01-07';
