33075c10a1946370a448c4b8205b21d96c77f4d5
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 01:22:07 +0300

    Update ex02.sql

--- day00/ex02.sql
+++ day00/ex02.sql
@@ -1,2 +1,2 @@
-SELECT name, rating FROM pizzeria where rating >= 3.5 and rating <=5;
-SELECT * FROM pizzeria ORDER BY rating;
+SELECT name, rating FROM pizzeria where rating >= 3.5 and rating <=5
+ORDER BY rating;
