827b7b57c52f59e985c14a08c45f109fc9e3c347
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 01:21:41 +0300

    Update ex01.sql

--- day00/ex01.sql
+++ day00/ex01.sql
@@ -1,3 +1,3 @@
 SELECT * from person;
-SELECT name, age FROM person WHERE gender = 'female' and adress = 'Kazan';
-SELECT * FROM person ORDER BY name;
+SELECT name, age FROM person WHERE gender = 'female' and adress = 'Kazan'
+ORDER BY name;
