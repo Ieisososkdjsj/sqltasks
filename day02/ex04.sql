6a3824c0a6293726511b0713027a2be9fc32748e
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 23:34:58 +0300

    Update ex04.sql

--- day02/ex04.sql
+++ day02/ex04.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    menu.pizza_name AS pizza_name,
+    pizzeria.name AS pizzeria_name,
+    menu.price AS price
+FROM menu
+JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+WHERE menu.pizza_name IN ('Margherita', 'pepperoni')
+ORDER BY pizza_name, pizzeria_name;
