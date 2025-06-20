65cdf4e1064e1c34123ce9f135a54d3019ee5055
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 21:58:41 +0300

    Update ex04.sql

--- day00/ex04.sql
+++ day00/ex04.sql
@@ -1,4 +1,4 @@
 SELECT 
-     name || ' (age:' || age || ',gender:' || '''' || gender || '''' || ',address:' || '''' || address || '''' || ')' AS person_information  
+     CONCAT(name, ' (age:', age, ',gender:', '''', gender, '''', ',address:', '''', address, '''', ')') AS person_information  
 FROM person
 ORDER BY person_information ASC;
