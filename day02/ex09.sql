34e154905a9dca49b572d7a89943a5a2a42ad813
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-21 01:56:40 +0300

    Update ex09.sql

--- day02/ex09.sql
+++ day02/ex09.sql
@@ -1,1 +1,15 @@
-
+SELECT name
+FROM person
+WHERE gender = 'female' AND id IN (
+    SELECT person_id
+    FROM person_order
+    JOIN menu ON person_order.menu_id = menu.id
+    WHERE pizza_name = 'pepperoni pizza'
+  )
+  AND id IN (
+    SELECT person_id
+    FROM person_order
+    JOIN menu ON person_order.menu_id = menu.id
+    WHERE pizza_name = 'cheese pizza'
+  )
+ORDER BY name ASC;
