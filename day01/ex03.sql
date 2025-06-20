2d9735e5f0883d26cb3668b321a6572137959811
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 18:17:37 +0300

    Update ex03.sql

--- day01/ex03.sql
+++ day01/ex03.sql
@@ -2,7 +2,5 @@ SELECT
     person_order.order_date AS action_date,
     person_order.person_id
 FROM person_order
-INNER JOIN person_visits
-    ON person_order.person_id = person_visits.person_id
-   AND person_order.order_date = person_visits.visit_date
+INNER JOIN person_visits ON person_order.person_id = person_visits.person_id AND person_order.order_date = person_visits.visit_date
 ORDER BY action_date ASC, person_id DESC;
