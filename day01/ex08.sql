c7961798bb8394c8965483c1466536c629913348
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 22:03:54 +0300

    Update ex08.sql

--- day01/ex08.sql
+++ day01/ex08.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    person_order.order_date AS order_date,
+    CONCAT(person.name, ' (', person.age, ')') AS person_info
+FROM 
+    person_order
+NATURAL JOIN 
+    person 
+ORDER BY 
+    person_order.order_date ASC, 
+    person.name ASC;
