f29c07d74fb7759d2fea1c04908dc2ca3424e89d
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-20 21:52:16 +0300

    Update ex07.sql

--- day01/ex07.sql
+++ day01/ex07.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    person_order.order_date AS order_date,
+    CONCAT(person.name, ' (', person.age, ')') AS person_info
+FROM 
+    person_order 
+JOIN 
+    person  ON person_order.person_id = person.id
+ORDER BY 
+    person_order.order_date ASC, 
+    person.name ASC;
