77667014ccb6cd0f61b3db49cca86e7d90dc06da
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-21 18:06:06 +0300

    Update ex13.sql

--- day03/ex13.sql
+++ day03/ex13.sql
@@ -1,1 +1,5 @@
-
+DELETE FROM person_order
+WHERE order_date = DATE '2022-02-25' AND menu_id IN (
+    SELECT id FROM menu WHERE pizza_name = 'greek pizza');
+DELETE FROM menu
+WHERE pizza_name = 'greek pizza';
