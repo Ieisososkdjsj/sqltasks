f0e111bfa90beef4c4c0ee8a3c4400412d36065f
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-21 16:19:21 +0300

    Update ex02.sql

--- day03/ex02.sql
+++ day03/ex02.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    menu.pizza_name AS pizza_name,
+    menu.price AS price,
+    pizzeria.name AS pizzeria_name
+FROM menu
+LEFT JOIN person_order ON menu.id = person_order.menu_id
+JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+WHERE person_order.menu_id IS NULL
+ORDER BY pizza_name, price;
