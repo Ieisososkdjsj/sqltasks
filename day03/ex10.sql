e638d83799eaea625ebec635d2f51412c1535438
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-21 17:53:32 +0300

    Update ex10.sql

--- day03/ex10.sql
+++ day03/ex10.sql
@@ -1,1 +1,9 @@
-
+INSERT INTO person_order (person_id, menu_id, order_date)
+VALUES (
+    (SELECT id FROM person WHERE name = 'Denis'),
+    (SELECT id FROM menu WHERE pizza_name = 'sicilian pizza'),
+    '2022-02-24'),
+  (
+    (SELECT id FROM person WHERE name = 'Irina'),
+    (SELECT id FROM menu WHERE pizza_name = 'sicilian pizza'),
+    '2022-02-24');
