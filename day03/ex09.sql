989cc488a63b2d9fde741382e82388ef3b912550
LordOfVampires <dmitrievatatiana22@gmail.com>
2025-06-21 17:39:47 +0300

    Update ex09.sql

--- day03/ex09.sql
+++ day03/ex09.sql
@@ -1,14 +1,11 @@
 NSERT INTO person_visits (id, person_id, pizzeria_id, visit_date)
-VALUES 
-  (
+VALUES (
     (SELECT MAX(id) + 1 FROM person_visits),
     (SELECT id FROM person WHERE name = 'Denis'),
     (SELECT id FROM pizzeria WHERE name = 'Dominos'),
-    '2022-02-24'
-  ),
+    '2022-02-24'),
   (
     (SELECT MAX(id) + 2 FROM person_visits),
     (SELECT id FROM person WHERE name = 'Irina'),
     (SELECT id FROM pizzeria WHERE name = 'Dominos'),
-    '2022-02-24'
-  );
+    '2022-02-24');
